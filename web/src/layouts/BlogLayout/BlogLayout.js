fed66d32ad0dcf47d0aaeeebfdc5df877aba80b7
Ryan Quinn <notryanquinn@gmail.com>
2025-01-28 16:33:25 -0800

    adding github action to auto update code 002

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -14,7 +14,7 @@ const BlogLayout = ({ children }) => {
             className="text-blue-400 hover:text-blue-300 transition duration-100"
             to={routes.home()}
           >
-            Testycloud 1.001
+            Testycloud 1.002
           </Link>
         </h1>
         <nav>
