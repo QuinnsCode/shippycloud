f352ddc07aeb3601902cd1f164bdd09d858e96e7
Ryan Quinn <notryanquinn@gmail.com>
2025-01-29 01:56:01 -0800

    re-adding changes after deployment failure 001

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,27 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: 'serve',
+//       cwd: 'current',
+//       script: 'node_modules/.bin/rw',
+//       args: 'serve',
+//       instances: 'max',
+//       exec_mode: 'cluster',
+//       wait_ready: true,
+//       listen_timeout: 10000,
+//     },
+//   ],
+// }
+// If you follow our recommended config below, you could update this to only serve the api side,
+// because the web side will be handled by nginx.
+
 module.exports = {
   apps: [
     {
-      name: 'serve',
+      name: 'api',
       cwd: 'current',
       script: 'node_modules/.bin/rw',
-      args: 'serve',
+      args: 'serve api',
       instances: 'max',
       exec_mode: 'cluster',
       wait_ready: true,
