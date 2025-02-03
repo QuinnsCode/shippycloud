a2a12d95fb041e6aaed46fb71426db442b9b5283
Ryan Quinn <notryanquinn@gmail.com>
2025-02-03 05:02:24 -0800

    addomg subscriptions002

--- web/src/components/UserSettingsUpdateWidget/UserSettingsUpdateWidget.jsx
+++ web/src/components/UserSettingsUpdateWidget/UserSettingsUpdateWidget.jsx
@@ -1,12 +1,28 @@
-const UserSettingsUpdateWidget = () => {
+import { useUserSettings } from 'src/hooks/useUserSettings'
+
+const UserSettingsUpdateWidget = ({ user }) => {
+  const { updateSetting, getSetting } = useUserSettings(
+    user.settings,
+    updateUserSettingsInDB
+  )
+
+  // Using a feature flag
+  const isNewFeatureEnabled = getSetting('newFeatureFlag', false)
+
+  // Enabling a new feature
+  const enableNewFeature = async () => {
+    const success = await updateSetting('newFeatureFlag', true)
+    if (success) {
+      console.log('New feature enabled')
+    } else {
+      console.log('Failed to enable new feature')
+    }
+  }
+
   return (
     <div>
-      <h2>{'UserSettingsUpdateWidget'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/UserSettingsUpdateWidget/UserSettingsUpdateWidget.jsx'
-        }
-      </p>
+      {isNewFeatureEnabled && <>New Feature Enabled</>}
+      <button onClick={enableNewFeature}>Enable New Feature</button>
     </div>
   )
 }
